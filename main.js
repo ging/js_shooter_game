0c3c7e5385267f2d2cc6e31a8c8c040b423a16b7
sonsoleslp <sonsoleslp@hotmail.com>
2019-11-25 21:10:20 +0100

    Refactor

---
+++ main.js
@@ -0,0 +1,29 @@
+const ENEMY_HEIGHT = 5,
+    ENEMY_SPEED = 5,
+    ENEMY_WIDTH = 10,
+    KEY_LEFT = "LEFT", // Percentage of screen width
+    KEY_RIGHT = "RIGHT", // Percentage of screen height
+    KEY_SHOOT = "SHOOT",
+    MIN_TOUCHMOVE = 20,
+    PLAYER_HEIGHT = 5,
+    PLAYER_SPEED = 20,
+    PLAYER_WIDTH = 10,
+    SHOT_HEIGHT = 3,
+    SHOT_WIDTH = 3;
+
+function getRandomNumber (range) {
+    return Math.floor(Math.random() * range);
+}
+
+function collision (div1, div2) {
+    const a = div1.getBoundingClientRect(),
+        b = div2.getBoundingClientRect();
+    return !(a.bottom < b.top || a.top > b.bottom || a.right < b.left || a.left > b.right);
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+        const game = new Game();
+        game.start();
+    }
+);
