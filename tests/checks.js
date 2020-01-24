e4cefcf9f4ed951481d4d57184d96ab2361f5884
sonsoleslp <sonsoleslp@hotmail.com>
2020-01-24 20:34:16 +0100

    Update tests

--- tests/checks.js
+++ tests/checks.js
@@ -11,23 +11,15 @@ const browser = new Browser({waitDuration: 100});
 const path_assignment = path.resolve(path.join(__dirname, "../index.html"));
 const URL = "file://"+path_assignment.replace("%", "%25");
 
-const to = function to(promise) {
-    return promise
-        .then(data => {
-            return [null, data];
-        })
-        .catch(err => [err]);
-}; 
-
 //TESTS
-describe("MOOC_game", function () {
+describe("Juego", function () {
 
     this.timeout(T_TEST * 1000);
 
-    it("1(Precheck): Checking that the assignment file exists...", async function () {
+    it("1(Precheck): Comprobando que existe el fichero de la entrega...", async function () {
         this.score = 0;
-        this.msg_ok = `Found the file '${path_assignment}'`;
-        this.msg_err = `Couldn't find the file '${path_assignment}'`;
+        this.msg_ok = `Encontrado el fichero '${path_assignment}'`;
+        this.msg_err = `No se encontró el fichero '${path_assignment}'`;
         const fileexists = await Utils.checkFileExists(path_assignment);
         if (!fileexists) {
             error_critical = this.msg_err;
@@ -35,138 +27,186 @@ describe("MOOC_game", function () {
         fileexists.should.be.equal(true);
     });
 
-    it("2(Precheck): Checking that the assignment file contains valid html...", async function () {
+    it("2(Precheck): Comprobando que el fichero contiene HTML válido...", async function () {
         this.score = 0;
         if (error_critical) {
             this.msg_err = error_critical;
 
         } else {
-            this.msg_ok = `'${path_assignment}' has been parsed correctly`;
-            this.msg_err = `Error parsing '${path_assignment}'`;
-            [error_nav, resp] = await to(browser.visit(URL));
+            this.msg_ok = `El fichero '${path_assignment}' se ha parseado correctamente`;
+            this.msg_err = `Error al parsear '${path_assignment}'`;
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
             if (error_nav) {
-                this.msg_err = `Error parsing '${path_assignment}': ${error_nav}`;
+                this.msg_err = `Error al parsear '${path_assignment}': ${error_nav}`;
                 error_critical = this.msg_err;
             }
         }
         should.not.exist(error_critical);
     });
 
-    it("3(Precheck): Checking 'Game' instance...", async function () {
+    it("3(Precheck): Comprobando que existe una instancia de la clase 'Game'...", async function () {
         this.score = 0;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
             if (error_nav) {
-                this.msg_err = `Couldn't find '${expected}' in ${path_assignment}\nError:${error_nav}\nReceived:${browser.text('body')}`;
+                 this.msg_err = `Error al abrir el fichero ${path_assignment}`;
             }
-            this.msg_ok = `Found game instance`;
-            this.msg_err = `Game is not started`;
+            this.msg_ok = `Instancia de clase 'Game' iniciada`;
+            this.msg_err = `No se encuentra instancia de la clase 'Game' iniciada`;
             browser.window.game.started.should.be.equal(true);
         }
     });
 
-    it("4: Checking 'Score' element exists...", async function () {
+    it("4: Comprobando que existe el elemento 'Score'...", async function () {
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
+            if (error_nav) {
+                this.msg_err = `Error al abrir el fichero ${path_assignment}
+                Error: ${error_nav}
+                Recibido: ${browser.text('body')}`;
+            }
+            this.msg_err = `No se encuentra ningún elemento 'li' con id 'scoreli'`;
+            await browser.wait({ duration: 500 });
             const scoreli = browser.html("li#scoreli");
-            this.msg_ok = "Found score"
-            this.msg_err = `List element with id "scoreli" contained ${ scoreli || "nothing"} instead of the score in the format specified in the task assignment`;
+            this.msg_ok = "Se ha encontrado el componente 'Score'";
+            this.msg_err = `El elemento 'li' con id 'scoreli'  ${ scoreli ? ("contiene '"+ scoreli +"'") : "esta vacío"} en vez de contener la puntuación en el formato especificado por el enunciado`;
             Boolean(scoreli.match(/Score: ?\d+/)).should.be.equal(true);
         }
     });
 
-    it("5: Checking 'Lives' element exists...", async function () {
+    it("5: Comprobando que existe el elemento 'Lives'...", async function () {
         this.score = 1;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
+
+            if (error_nav) {
+                this.msg_err = `Error al abrir el fichero ${path_assignment}
+                Error: ${error_nav}
+                Recibido: ${browser.text('body')}`;
+            }
+            await browser.wait({ duration: 500 });
+            this.msg_err = `No se encuentra ningún elemento 'li' con id 'livesli'`;
             const livesli = browser.html("li#livesli");
-            this.msg_ok = "Found lives"
-            this.msg_err = `List element with id "livesli" contained ${ livesli || "nothing"} instead of the lives in the format specified in the task assignment`;
+            this.msg_ok = "Se ha encontrado el componente 'Lives'";
+            this.msg_err = `El elemento 'li' con id 'livesli'  ${ livesli ? ("contiene '"+ livesli + "'") : "esta vacío"} en vez de contener el número de vidas en el formato especificado por el enunciado`;
+            this.msg_ok = "Se ha encontrado el componente 'Lives'";
             Boolean(livesli.match(/Lives: ?\d+/)).should.be.equal(true);
         }
     });
 
-    it("6: Checking 'Score' works...", async function () {
+    it("6: Comprobando que el 'Score' funciona...", async function () {
         this.score = 1.5;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
+
+            if (error_nav) {
+                this.msg_err = `Error al abrir el fichero ${path_assignment}
+                Error: ${error_nav}
+                Recibido: ${browser.text('body')}`;
+            }
+            this.msg_err = "La funcionalidad de 'Score' no ha sido implementada";
+            await browser.wait({ duration: 500 });
             const scoreInitial = parseInt(browser.html("li#scoreli").match(/Score: ?(\d+)/)[1]);
+            this.msg_err = "No se encuentra la instancia de 'Game' en el JavaScript";
             const { game } = browser.window;
+            this.msg_err = "Ha fallado el método 'die' del oponente";
             game.opponent.die();
             await browser.wait({ duration: 2500 });
             const scoreFinal = parseInt(browser.html("li#scoreli").match(/Score: ?(\d+)/)[1]);
-            this.msg_ok = "Score increased after shooting opponent";
-            this.msg_err = "Score did not increase after shooting opponent";
+            this.msg_ok = "El campo 'Score' ha aumentado tras disparar al oponente";
+            this.msg_err = "El campo 'Score' NO ha aumentado tras disparar al oponente";
             scoreFinal.should.be.equal(scoreInitial + 1);
         }
     });
 
-    it("7: Checking 'Lives' works...", async function () {
+    it("7: Comprobando que 'Lives' funciona...", async function () {
         this.score = 1.5;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
 
+            if (error_nav) {
+                this.msg_err = `Error al abrir el fichero ${path_assignment}
+                Error: ${error_nav}
+                Recibido: ${browser.text('body')}`;
+            }
+            this.msg_err = "La funcionalidad de 'Lives' no ha sido implementada";
             const livesInitial = parseInt(browser.html("li#livesli").match(/Lives: ?(\d+)/)[1]);
+            this.msg_err = "No se encuentra la instancia de 'Game' en el JavaScript";
             const { game } = browser.window;
+            this.msg_err = "Ha fallado el método 'die' del jugador";
             game.player.die();
             await browser.wait({ duration: 1000 });
-            this.msg_ok = "Player lost a life after being shot";
-            this.msg_err = "Player did not lose a life after being shot"
             const livesFinal = parseInt(browser.html("li#livesli").match(/Lives: ?(\d+)/)[1]);
+            this.msg_ok = "El jugador ha perdido una vida tras ser disparado";
+            this.msg_err = "El jugador NO ha perdido una vida tras ser disparado";
             livesInitial.should.be.equal(livesFinal + 1);
         }
     });
 
-    it("8: Checking Boss appears after opponent dies...", async function () {
+    it("8: Comprobando que una instancia de 'Boss' aparece cuando muere el oponente...", async function () {
         this.score = 2.5;
         if (error_critical) {
             this.msg_err = error_critical;
             should.not.exist(error_critical);
         } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+            [error_nav, resp] = await Utils.to(browser.visit(URL));
 
-            const livesInitial = parseInt(browser.html("li#livesli").match(/Lives: ?(\d+)/)[1]);
+            if (error_nav) {
+                this.msg_err = `Error al abrir el fichero ${path_assignment}
+                Error: ${error_nav}
+                Recibido: ${browser.text('body')}`;
+            }
+            this.msg_err = "No se encuentra la instancia de 'Game' en el JavaScript";
             const { game } = browser.window;
+            this.msg_err = "Ha fallado el método 'die' del jugador";
             game.opponent.die();
             await browser.wait({ duration: 3000 });
-            this.msg_ok = "Boss shows up after opponent is killed";
-            this.msg_err = "Boss does not show up after opponent is killed";
+            this.msg_ok = "El 'Boss' aparece tras matar al oponente";
+            this.msg_err = "El 'Boss' NO aparece tras matar al oponente";
             game.opponent.constructor.name.should.be.equal("Boss");
         }
 
     });
-    it("9: Checking Boss's speed...", async function () {
+    it("9: Comprobando la velocidad del 'Boss'...", async function () {
         this.score = 2.5;
         if (error_critical) {
-            this.msg_err = error_critical;
-            should.not.exist(error_critical);
-        } else {
-            [error_nav, resp] = await to(browser.visit(URL));
+           this.msg_err = error_critical;
+           should.not.exist(error_critical);
+       } else {
+           [error_nav, resp] = await Utils.to(browser.visit(URL));
 
-            const livesInitial = parseInt(browser.html("li#livesli").match(/Lives: ?(\d+)/)[1]);
+           if (error_nav) {
+               this.msg_err = `Error al abrir el fichero ${path_assignment}
+               Error: ${error_nav}
+               Recibido: ${browser.text('body')}`;
+           }
+
+            this.msg_err = "No se encuentra la instancia de 'Game' en el JavaScript";
             const { game } = browser.window;
+            this.msg_err = "El oponente no tiene atributo 'speed'";
             const opponentSpeed = game.opponent.speed;
+            this.msg_err = "Ha fallado el método 'die' del oponente";
             game.opponent.die();
             await browser.wait({ duration: 3000 });
             const bossSpeed = game.opponent.speed;
-            this.msg_ok = "Boss moves twices as fast as the opponent";
-            this.msg_err = "Boss does not move twice as fast as the opponent";
+            this.msg_ok = "El 'Boss' se mueve al doble de velocidad que el oponente";
+            this.msg_err =  "El 'Boss' NO se mueve al doble de velocidad que el oponente";
             bossSpeed.should.be.equal(opponentSpeed*2);
         }
 
